44e08baa4860d42654dc198cb0edd70afbdfd29c
rmisca <rominamisca95@gmail.com>
2021-03-07 20:50:37 +0200

    Created batch-view component

---
+++ src/app/pages/batch-view/batch-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-batch-view',
+  templateUrl: './batch-view.component.html',
+  styleUrls: ['./batch-view.component.scss']
+})
+export class BatchViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
